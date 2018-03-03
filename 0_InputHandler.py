b3eca848391206f7237189e9e6bfdaae14a9c662
Alex <msumoskalenko@gmail.com>
2018-03-03 19:46:28 +0300

    Input Handler

---
+++ 0_InputHandler.py
@@ -0,0 +1,14 @@
+from Tkinter import *
+
+master = Tk()
+e = Entry(master)
+e.pack()
+e.focus_set()
+
+def callback():
+    print e.get()
+
+b = Button(master, text="get", width=10, command=callback)
+b.pack()
+
+mainloop()
