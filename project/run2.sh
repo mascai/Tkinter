d46ffe5709523cdaa528756ffe6089bdeee82330
Alex <msumoskalenko@gmail.com>
2018-03-11 19:03:19 +0300

    Update run2.sh

--- project/run2.sh
+++ project/run2.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sudo mn --topo single,3 --mac --controller remote --switch ovsk
+#~/pox/pox.py openflow.of_01 --port=6653 forwarding.l2_learning 
+~/pox/pox.py openflow.of_01 --port=6667 example
+
